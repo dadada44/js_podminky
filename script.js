691c122cbe6b572fea5a8a6380eb164a610e6018
Daryna Toporovska <Daryna.Toporovska@student.souepl.cz>
2024-11-15 11:34:38 +0100

    hotovo asi

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
+/*
 function Control(){
     const password = document.getElementById("password");
     const password_control = document.getElementById("control_password");
+    const code = document.getElementById("code");
 
     //console.log(password.value);
 
     //console.log("Password = ", password, "Password_control = ", password_control)
 
     if (password.value == password_control.value){
-        console.log("Login is true")
-        document.getElementById("output").innerHTML = "Login is true";
+
+        if (!isNaN(code.value)){
+            console.log("Login is true")
+            document.getElementById("output").innerHTML = "Login is true";
+        }
+        else{
+            document.getElementById("output").innerHTML = "Login is false";
+        }
+    }
+    else{
+        console.log("Login is false")
+        document.getElementById("output").innerHTML = "Login is false";
+    }
+}
+*/
+
+function Control(){
+    const password = document.getElementById("password");
+    const password_control = document.getElementById("control_password");
+    const code = document.getElementById("code");
+    const nickname = document.getElementById("nickname")
+
+
+    if ((password.value == password_control.value) && (!isNaN(code.value))){
+        if((nickname.value !="") && (password != "") && (password_control != "") && (code != "")){
+            console.log("Login is true")
+            document.getElementById("output").innerHTML = "Login is true";
+        }
+        else{
+            console.log("Any input is empty")
+            document.getElementById("output").innerHTML = "Any input is empty";
+        }
     }
     else{
         console.log("Login is false")
